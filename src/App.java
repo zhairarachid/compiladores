cea03816b308322968b93c510e0fd6688564467e
zhairarachid <zhairarachid@gmail.com>
2023-05-07 14:17:24 -0400

    Initial commit 	new file:   src/App.java 	.vscode/ 	README.md 	bin/ 	gramatica.txt 	src/Lexico.java 	src/Sintatico.java 	src/Token.java

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class App{
+    public static void main(String[] args) throws Exception{
+        ArrayList<Token> TokenLista = new ArrayList<>();
+
+        TokenLista = Lexico.main();
+        if(!TokenLista.isEmpty()){
+            
+        }
+    }
+}
